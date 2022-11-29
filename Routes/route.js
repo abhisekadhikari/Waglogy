bf875b81dcbfa991c428efd9757867ea19fd461d
Abhisek Adhikari <abhisekadhikari1906@gmail.com>
2022-11-29 23:18:59 +0530

    Updating Information

--- Routes/route.js
+++ Routes/route.js
@@ -2,16 +2,7 @@ const express = require('express')
 const route = express.Router()
 
 route.get('/', (req, res) => {
-    res.render('index', {
-        name: "Admin"
-    })
-})
-
-route.post("/post", (req, res) => {
-    data = req.body.name
-    role = req.body.role
-    console.log(data , role)
-    res.status(200).json({status: "ok"})
+    res.render('index')
 })
 
 module.exports = route
