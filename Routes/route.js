0bf57b7824938c0181c3828716665b46c2f1445f
abhisekadhikari <89593428+abhisekadhikari@users.noreply.github.com>
2023-05-30 14:52:15 +0530

    Waglogy v2

--- Routes/route.js
+++ Routes/route.js
@@ -1,131 +1,25 @@
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const route = express.Router();
-const Contact = require("../Model/Schema");
-const Feedback = require("../Model/Feedback");
-const Admin = require("../Model/Admin");
-const { check, validationResult } = require("express-validator");
-const auth = require("./auth");
+const express = require("express")
+const jwt = require("jsonwebtoken")
+const route = express.Router()
+const Contact = require("../Model/Schema")
+const Feedback = require("../Model/Feedback")
+const Admin = require("../Model/Admin")
+const { check, validationResult } = require("express-validator")
 
-route.get("/", async (req, res) => {
-  const data = await Feedback.find({}).lean();
-  res.render("index", {
-    data,
-  });
-});
-
-route.get("/contact", (req, res) => {
-  res.render("contact");
-});
-
-route.get("/services", (req, res) => {
-  res.render("services");
-});
-
-route.get("/studentc", (req, res) => {
-  res.render("studentc");
-});
+route.get("/", (req, res) => {
+  res.render("index")
+})
 
 route.get("/about", (req, res) => {
-  res.render("about");
-});
-route.get("/privicy", (req, res) => {
-  res.render("privicy");
-});
-route.post(
-  "/api/contact",
-  [
-    check("name", "Name Cannot Be Empty").exists().isLength({ min: 5 }),
-    check("email", "Email is not valid").isEmail().normalizeEmail(),
-    check("number", "Enter valid number").exists().isNumeric(),
-    check("message", "Message cannot be empty").exists().isLength({ min: 5 }),
-  ],
-  (req, res) => {
-    const { name, number, email, message } = req.body;
-    const result = validationResult(req);
-    if (!result.isEmpty()) {
-      const alert = result.array();
-      return res.render("contact", {
-        alert,
-      });
-    }
-    const data = new Contact({ name, email, number, message });
-    data.save();
-    res.status(200).redirect("/contact");
-  }
-);
+  res.render("about")
+})
 
-route.post(
-  "/api/feedback",
-  [
-    check("name", "Name Cannot Be Empty").exists().isLength({ min: 5 }),
-    check("email", "Email is not valid").isEmail().normalizeEmail(),
-    check("message", "Mesage Cannot Be Empty").exists().isLength({ min: 5 }),
-  ],
-  (req, res) => {
-    const { name, message, email } = req.body;
+route.get("/services", (req, res) => {
+  res.render("service")
+})
 
-    const result = validationResult(req);
-    if (!result.isEmpty()) {
-      const alert = result.array();
-      res.render("index", {
-        alert,
-      });
-    } else {
-      const data = new Feedback({ name, message, email });
-      data.save();
-      res.status(200).redirect("/");
-    }
-  }
-);
+route.get("/contact", (req, res) => {
+  res.render("contact")
+})
 
-route.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res.status(404).json({ message: "Enter All The Filds" });
-  }
-  const data = await Admin.findOne({ email: email });
-  try {
-    if (!data) {
-      return res.status(404).json({ message: "Craniantials Failed" });
-    }
-    if (data.password != password) {
-      return res.status(404).json({ message: "Enter valid Cradentials" });
-    }
-    const ID = data._id.toString();
-    const token = jwt.sign(ID, `${process.env.SECRET_KEY}`);
-    res
-      .cookie("jwt", token, {
-        expires: new Date(Date.now() + 300000),
-        httpOnly: true,
-        secure: true,
-      })
-      .status(200)
-      .redirect("/admin");
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-route.get("/login", (req, res) => {
-  res.render("login");
-});
-
-route.get("/admin", auth, async (req, res) => {
-  const id = jwt.verify(req.cookies.jwt, process.env.SECRET_KEY);
-  const admin = await Admin.findOne({ _id: id });
-  const feedbackData = await Feedback.find({}).lean();
-  const contactData = await Contact.find({}).lean();
-  const name = admin.name;
-  res.render("admin", {
-    name,
-    feedbackData,
-    contactData,
-  });
-});
-
-route.get("*", (req, res) => {
-  res.render("nopage");
-});
-
-module.exports = route;
+module.exports = route
--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const { engine } = require("express-handlebars");
-const mongoose = require("mongoose");
-require("dotenv").config();
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static("public"));
-app.engine(".hbs", engine({ extname: ".hbs" }));
-app.set("view engine", ".hbs");
-app.set("views", "./views");
-app.use(require("./Routes/route"));
-const auth = require("./Routes/auth");
-DB_link = process.env.MONGO;
-port = process.env.PORT || 3000;
+const express = require("express")
+const path = require("path")
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const { engine } = require("express-handlebars")
+const mongoose = require("mongoose")
+require("dotenv").config()
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static("public"))
+app.engine(".hbs", engine({ extname: ".hbs" }))
+app.set("view engine", ".hbs")
+app.set("views", "./views")
+app.use(require("./Routes/route"))
+DB_link = process.env.MONGO
+port = process.env.PORT || 3000
 mongoose
   .connect(DB_link)
   .then(() => {
-    console.log("Connection Successful");
+    console.log("Connection Successful")
   })
   .catch((err) => {
     if (err) {
-      console.log("Connection Unsuccessful");
+      console.log("Connection Unsuccessful")
     }
-  });
+  })
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running on port ${port}`)
+})
