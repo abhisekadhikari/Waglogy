71fa45a466227f423684a5015211798e13ebeb44
abhisekadhikari <abhisekadhikari1906@gmail.com>
2024-01-12 13:19:39 +0530

    Update 3.0

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express, { urlencoded } from "express"
 import route from "./routes/route.js"
 import cors from "cors"
 import path from "path"
-import ApiError from "./utils/ApiError.js"
+import globalErrorMiddleware from "./middlewares/globalErrorMiddleware.js"
 const app = express()
 const __dirname = path.resolve()
 app.use(
@@ -22,13 +22,6 @@ app.use(express.static(path.join(__dirname, "/src", "/public")))
 
 app.use(route)
 
-app.use((err, req, res, next) => {
-  if (err instanceof ApiError) {
-    return res.status(err.statusCode).json({ message: err.message })
-  }
-  return res.status(500).json({
-    message: "Internal Server Error.",
-  })
-})
+app.use(globalErrorMiddleware)
 
 export { app }
---
+++ src/middlewares/globalErrorMiddleware.js
@@ -0,0 +1,12 @@
+import ApiError from "../utils/ApiError.js"
+
+const globalErrorMiddleware = (err, req, res, next) => {
+  if (err instanceof ApiError) {
+    return res.status(err.statusCode).json({ message: err.message })
+  }
+  return res.status(500).json({
+    message: "Internal Server Error.",
+  })
+}
+
+export default globalErrorMiddleware
