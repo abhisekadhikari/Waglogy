1cc28252652fbe95e348bdab971af011a089dbe3
abhisekadhikari <abhisekadhikari1906@gmail.com>
2024-01-16 19:06:33 +0530

    🚀 prod: Production

--- src/utils/ApiResponse.js
+++
@@ -1,9 +0,0 @@
-class ApiResponse {
-    constructor(statusCode, data, message = "Success") {
-        this.statusCode = statusCode
-        this.data = data
-        this.message = message
-        this.success = statusCode < 400
-    }
-}
-export default ApiResponse
